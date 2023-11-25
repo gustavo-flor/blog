998fbe4add159852db1777acb73144ec4dbe2aae
gustavo-flor <gustavo.flor@picpay.com>
2023-11-25 10:04:33 -0300

    :wrench: Adding deploy configs

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,18 +1,18 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 import Article from './pages/Article';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 
 const Router = () => (
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/posts' element={<Home />} />
       <Route path='/posts/:slug' element={<Article />} />
       <Route path='*' element={<NotFound />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 export default Router;
