88cf7b51fa113c22240cfa9aa263e77bd125f89e
gustavo-flor <gustavo.flor@picpay.com>
2023-11-26 17:52:04 -0300

    :beers: Doing some thing

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
+import { Analytics } from '@vercel/analytics/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App.tsx';
+
 
 const root = document.getElementById('root')!;
 
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
