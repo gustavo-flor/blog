69ff46e014d108555428f1ebd6bf7f8d331629e0
gustavo-flor <gustavo.flor@picpay.com>
2023-11-26 09:04:49 -0300

    :wrench: Adjusting deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.md']
+  assetsInclude: ['**/*.md'],
+  base: '/blog'
 });
