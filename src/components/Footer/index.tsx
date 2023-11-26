eaa176f8aead10b30403eaf901f35c48c82aebd1
gustavo-flor <gustavo.flor@picpay.com>
2023-11-26 15:23:57 -0300

    :beers: Doing some thing

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,20 +2,6 @@ import Brand from '../Brand';
 
 const Footer = () => (
   <footer className='w-full bg-zinc-900 text-gray-200'>
-    {/* <div>
-      <h4>Conecte-se</h4>
-      <ul>
-        <li><a href="https://github.com/gustavo-flor">Github (@gustavo-flor)</a></li>
-        <li><a href="https://instagram.com/gustavoflor_">Instagram (@gustavoflor_)</a></li>
-        <li><a href="https://www.linkedin.com/in/gustavoflor/">LinkedIn (@gustavoflor)</a></li>
-      </ul>
-    </div>
-    <div>
-      <h4>Créditos</h4>
-      <ul>
-        <li><a href="https://www.freepik.com/icon/rose_2926754">Ícones por Triberion</a></li>
-      </ul>
-    </div> */}
     <div className='container mx-auto p-8'>
       <p><Brand /> &copy; {new Date().getFullYear()}. Alguns direitos reservados.</p>
     </div>
