50b14dbd4d2709ad66f53c4960fd3e2dc4a6592b
gustavo-flor <gustavo.flor@picpay.com>
2023-11-26 15:53:21 -0300

    :beers: Doing some thing

--- src/components/Presentation/index.tsx
+++ src/components/Presentation/index.tsx
@@ -2,7 +2,7 @@
 const Presentation = () => (
   <main className='border-b'>
     <div className='container mx-auto flex items-center px-8 py-16 md:py-20'>
-      <div className='md:w-2/3'>
+      <div className='lg:w-2/3'>
         <h1 className='text-3xl text-gray-400'>
           Olá, este é o lugar em que <strong className='text-purple-500'>escrevo o que gostaria de ler</strong>, falo sobre o que estou estudando e publico <s>devaneios</s> sobre sistemas que você utiliza todos os dias. 🦎
         </h1>
