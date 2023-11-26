270caabdf7ace267affc9ba2fbb5f0782e624b33
gustavo-flor <gustavo.flor@picpay.com>
2023-11-26 16:57:49 -0300

    :beers: Doing some thing

--- src/components/LastPosts/index.tsx
+++ src/components/LastPosts/index.tsx
@@ -5,7 +5,7 @@ const LastPosts = () => {
   const posts = findAll();
 
   return (
-    <section className='container mx-auto px-8 py-16 md:py-20'>
+    <section className='container mx-auto px-8 py-8 sm:py-16 md:py-20'>
       <h3 className='text-2xl'>Veja as <strong>últimas publicações</strong></h3>
       <ul className='flex flex-wrap justify-between gap-y-8 sm:gap-y-16 mt-8'>{posts.map(post => 
         <li key={post.slug} className='md:w-[47.5%] lg:w-[30%]'><Card post={post} /></li>
--- src/components/Presentation/index.tsx
+++ src/components/Presentation/index.tsx
@@ -2,8 +2,8 @@ import Me from './../../assets/me.jpg';
 
 const Presentation = () => (
   <main>
-    <div className='border-b flex flex-col items-center mb-10 py-10'>
-      <div className='container mx-auto flex items-center px-8 py-12 sm:py-16 md:py-20'>
+    <div className='border-b flex flex-col items-center mb-10 pb-10'>
+      <div className='container mx-auto flex items-center px-8 py-8 sm:py-16 md:py-20'>
         <h1 className='lg:w-2/3 text-2xl sm:text-3xl text-gray-400'>
           Olá, este é o lugar em que <strong className='text-purple-500'>escrevo o que gostaria de ler</strong>, falo sobre o que estou estudando e publico <s>devaneios</s> sobre sistemas que você utiliza todos os dias. 🦎
         </h1>
