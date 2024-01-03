f013fb3943b683ea39440ee3f0168dcdedb42c81
gustavo-flor <gustavo.flor@picpay.com>
2024-01-03 14:00:55 -0300

    :memo: Adding 'Distributed Tracing' story

--- src/components/Tags/index.tsx
+++ src/components/Tags/index.tsx
@@ -6,13 +6,14 @@ interface Props {
 
 const Tags = ({ tags }: Props) => {
   const { tag: query = '' } = useParams();
+  const getLink = (tag: string) => tag != query ? `/tags/${tag}` : '/';
   const getStyle = (tag: string) => tag != query 
     ? ' opacity-40 hover:opacity-100 hover:rounded-xl transition-all' 
     : '';
   return (
     <ul className='flex flex-wrap gap-x-2'>{tags.map(tag =>
       <li key={tag}>
-        <Link to={`/tags/${tag}`}>
+        <Link to={getLink(tag)}>
           <div className={`rounded-2xl bg-gray-50 text-zinc-700 px-2 py-1 ${getStyle(tag)}`}>
             <strong className='font-normal text-purple-500'>#</strong>{tag}
           </div>
