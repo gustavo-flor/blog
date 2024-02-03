4751d805dac984fdaa9baea1d3309c642ad24da3
gustavo-flor <gustavo.flor@picpay.com>
2024-02-03 07:44:26 -0300

    :wrench: Improving SEO and other lighthouse issues

--- src/components/AppBar/index.tsx
+++ src/components/AppBar/index.tsx
@@ -13,7 +13,7 @@ const AppBar = ({ alwaysCenter = false }: Props) => {
     <header className='bg-zinc-900'>
       <nav className={`container mx-auto p-8 flex justify-center ${justify}`}>
         <Link to='/' className='flex gap-2 items-center hover:opacity-80'>
-          <img src={Lotus} width={32} />
+          <img src={Lotus} width={32} alt='Lotus Icon' />
           <Brand className='text-2xl' />
         </Link>
       </nav>
--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -10,7 +10,7 @@ const Card = ({ post }: Props) => (
   <div>
     <Tags tags={post.tags} />
     <Link to={`/posts/${post.slug}`}>
-      <h4 className='text-xl font-bold hover:opacity-60 my-2'>{post.title}</h4>
+      <h3 className='text-xl font-bold hover:opacity-60 my-2'>{post.title}</h3>
     </Link>
     <p className='opacity-80'>{post.description}</p>
   </div>
