8d19b53bc5aaa5f18b6be23a732c84b669c7565d
Gustavo Flôr <ogustaflor@gmail.com>
2024-12-15 19:14:45 -0300

    :sparkles: Adding internationalization (i18n)

--- src/services/lang.ts
+++ src/services/lang.ts
@@ -1,3 +1,10 @@
-export const getLanguage = (defaultValue: string = "pt-BR") => {
+export enum Languages {
+  PT_BR = "pt-BR",
+  EN_US = "en-US",
+}
+
+export const getDefaultLanguage = () => Languages.PT_BR
+
+export const getLanguage = (defaultValue: Languages = getDefaultLanguage()) => {
   return defaultValue
 }
