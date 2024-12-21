c2ebd59ef0a2af613e0c3e655429881705c4f212
Gustavo Flôr <ogustaflor@gmail.com>
2024-12-20 21:54:15 -0300

    :sparkles: Adding some translations

--- src/services/post.ts
+++ src/services/post.ts
@@ -155,7 +155,7 @@ const circuitBreaker: Post = {
   fileName: '50-circuit-breaker',
   createdAt: new Date(2024, 4, 13, 6, 0),
   listed: true,
-  availableLanguages: [LanguageCode.PT_BR]
+  availableLanguages: [LanguageCode.PT_BR, LanguageCode.EN_US]
 }
 
 const sdkman: Post = {
@@ -200,7 +200,7 @@ const springMail: Post = {
   fileName: '47-spring-mail',
   createdAt: new Date(2024, 3, 8, 6, 0),
   listed: true,
-  availableLanguages: [LanguageCode.PT_BR]
+  availableLanguages: [LanguageCode.PT_BR, LanguageCode.EN_US]
 }
 
 const concurrencyAndParallelism: Post = {
