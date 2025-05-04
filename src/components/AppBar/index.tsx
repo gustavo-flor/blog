0a20225aca93bfc8d38919d345192000336ab4c0
Gustavo Flôr <ogustaflor@gmail.com>
2025-05-04 19:57:14 -0300

    :beers: Page Reader

--- src/components/AppBar/index.tsx
+++ src/components/AppBar/index.tsx
@@ -11,7 +11,7 @@ const AppBar = ({ center }: AppBarProps) => {
     <header className='w-full bg-zinc-900 text-gray-200'>
       <nav className={`container mx-auto p-8 flex items-center ${center ? 'justify-center' : ''}`}>
         <Link href='/' className='flex gap-2 items-center hover:opacity-80'>
-          <img src="/images/lotus.svg" width={32} height={32} alt='Lotus Icon' />
+          <img src='/images/lotus.svg' width={32} height={32} alt='Lotus Icon' />
           <Brand className='text-2xl' />
         </Link>
       </nav>
--- src/components/Presentation/index.tsx
+++ src/components/Presentation/index.tsx
@@ -8,7 +8,7 @@ const Presentation = () => {
           </h1>
         </div>
         <div className='w-20 h-20 flex relative items-center rounded-full overflow-hidden border-2 mb-[-5rem] transition-all hover:scale-150 cursor-pointer'>
-          <img src="/images/me-in-purple.jpg" alt="Fotografia do autor" />
+          <img src='/images/me-in-purple.jpg' alt='Fotografia do autor' />
         </div>
       </div>
     </main>
